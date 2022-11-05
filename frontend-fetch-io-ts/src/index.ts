f4aec31ecec2f9ae38fd643b10215bf511162c02
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2022-11-05 19:38:51 +0200

    #5 Using 'export type' on type-only packages to a…
    …void runtime errors.

--- frontend-fetch-io-ts/src/index.ts
+++ frontend-fetch-io-ts/src/index.ts
@@ -1,4 +1,21 @@
-export * from "@ty-ras/protocol";
+// Really waiting for that "export type *": https://github.com/microsoft/TypeScript/issues/37238
+// If we just do "export * from", our index.[m]js file ends up with "export" statement as well, thus causing runtime errors.
+// Another option is rename .d.ts files into .ts files and end up with a bunch of empty .[m]js files and index.[m]js exporting those - not very optimal either.
+export type {
+  Encoded,
+  EncodedOf,
+  GetRuntimeArray,
+  GetRuntimeObject,
+  HKTEncodedBase,
+  ProtocolSpecCore,
+  ProtocolSpecHeaderData,
+  ProtocolSpecHeaders,
+  ProtocolSpecQuery,
+  ProtocolSpecRequestBody,
+  ProtocolSpecResponseHeaders,
+  ProtocolSpecURL,
+  RuntimeOf,
+} from "@ty-ras/protocol";
 export * from "@ty-ras/client-fetch";
 export * from "@ty-ras/data";
 export * from "@ty-ras/data-io-ts";
