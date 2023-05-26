341ffef219442ace6d8495b9dcf7c6a527ae878a
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2023-05-26 16:00:39 +0300

    Release of BE-NODE-IOTS-OPENAPI version 1.0.0.

--- backend-node-io-ts-openapi/src/index.ts
+++ backend-node-io-ts-openapi/src/index.ts
@@ -1,21 +1,12 @@
-// Really waiting for that "export type *": https://github.com/microsoft/TypeScript/issues/37238
-// If we just do "export * from", our index.[m]js file ends up with "export" statement as well, thus causing runtime errors.
-// Another option is rename .d.ts files into .ts files and end up with a bunch of empty .[m]js files and index.[m]js exporting those - not very optimal either.
-export type {
-  Encoded,
-  EncodedOf,
-  GetRuntimeArray,
-  GetRuntimeObject,
-  HKTEncodedBase,
-  ProtocolSpecCore,
-  ProtocolSpecHeaderData,
-  ProtocolSpecHeaders,
-  ProtocolSpecQuery,
-  ProtocolSpecRequestBody,
-  ProtocolSpecResponseHeaders,
-  ProtocolSpecURL,
-  RuntimeOf,
-} from "@ty-ras/protocol";
+/**
+ * @file This file exports all of the types, functions, and constants of the TyRAS libraries aimed for:
+ * - backend support
+ * - with bare Node HTTP server
+ * - using `io-ts` as data validation framework
+ * - using OpenAPI as supported metadata format
+ */
+
+export type * from "@ty-ras/protocol";
 export * from "@ty-ras/endpoint";
 export * from "@ty-ras/endpoint-prefix";
 export * from "@ty-ras/endpoint-spec";
@@ -25,17 +16,10 @@ export * from "@ty-ras/data";
 export * from "@ty-ras/data-io-ts";
 export * from "@ty-ras/data-backend";
 export * from "@ty-ras/data-backend-io-ts";
-export type {
-  EndpointMetadataInformation,
-  GetEndpointsMetadata,
-  HKTArg,
-  Kind,
-  MetadataProvider,
-  MetadataProviderForEndpoints,
-  SingleEndpointResult,
-  URLParameterSpec,
-  URLParametersInfo,
-} from "@ty-ras/metadata";
+export type * from "@ty-ras/metadata";
 export * from "@ty-ras/metadata-openapi";
 export * from "@ty-ras/metadata-jsonschema";
 export * from "@ty-ras/metadata-jsonschema-io-ts";
+
+// Export "glue" code, needed because e.g. OpenAPI MD library is not aware of IO-TS data validation library, and IO-TS data validation library is not aware of OpenAPI MD library.
+export * from "./md";
