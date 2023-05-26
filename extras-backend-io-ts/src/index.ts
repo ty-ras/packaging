acdb9de2f532b3b0c02515cf03747129c885f67b
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2023-05-26 23:00:33 +0300

    #42 Releasing 1.0.0 for IO-TS specific extras pac…
    …kages.

--- extras-backend-io-ts/src/index.ts
+++ extras-backend-io-ts/src/index.ts
@@ -4,7 +4,10 @@
  * - using `io-ts` as data validation framework
  */
 
-export * as configuration from "@ty-ras-extras/config-io-ts";
+import * as configString from "@ty-ras-extras/config-io-ts/string";
+import * as configMaybeFile from "@ty-ras-extras/config-io-ts/maybe-file";
+
+export const configuration = { ...configString, ...configMaybeFile };
 export * as cache from "@ty-ras-extras/cache";
 export * as resources from "@ty-ras-extras/resource-pool-fp-ts";
 export * as sql from "@ty-ras-extras/typed-sql-io-ts";
