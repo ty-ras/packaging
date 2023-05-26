4b251ecd5847acaded5b3e3baa0f6b3c72646d4a
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2023-05-26 16:18:22 +0300

    Release of FE-FETCH-IOTS version 1.0.0.

---
+++ frontend-fetch-io-ts/src/api-call.ts
@@ -0,0 +1,14 @@
+/**
+ * @file This file provides QoL function to create API call factory.
+ */
+
+import * as client from "@ty-ras/client-fetch";
+import * as data from "@ty-ras/data-frontend-io-ts";
+
+/**
+ * Creates {@link data.APICallFactory} that can be used to create callbacks which will call specific HTTP endpoint.
+ * @param args The {@link client.HTTPEndpointCallerArgs} to use.
+ * @returns A {@link data.APICallFactory} that can be used to create callbacks which will call specific HTTP endpoint.
+ */
+export const createAPICallFactory = (args: client.HTTPEndpointCallerArgs) =>
+  data.createAPICallFactoryWithCallback(client.createCallHTTPEndpoint(args));
--- frontend-fetch-io-ts/src/index.ts
+++ frontend-fetch-io-ts/src/index.ts
@@ -1,23 +1,16 @@
-// Really waiting for that "export type *": https://github.com/microsoft/TypeScript/issues/37238
-// If we just do "export * from", our index.[m]js file ends up with "export" statement as well, thus causing runtime errors.
-// Another option is rename .d.ts files into .ts files and end up with a bunch of empty .[m]js files and index.[m]js exporting those - not very optimal either.
-export type {
-  Encoded,
-  EncodedOf,
-  GetRuntimeArray,
-  GetRuntimeObject,
-  HKTEncodedBase,
-  ProtocolSpecCore,
-  ProtocolSpecHeaderData,
-  ProtocolSpecHeaders,
-  ProtocolSpecQuery,
-  ProtocolSpecRequestBody,
-  ProtocolSpecResponseHeaders,
-  ProtocolSpecURL,
-  RuntimeOf,
-} from "@ty-ras/protocol";
+/**
+ * @file This file exports all of the types, functions, and constants of the TyRAS libraries aimed for:
+ * - frontend support
+ * - with `fetch` as HTTP client
+ * - using `io-ts` as data validation framework
+ */
+
+export type * from "@ty-ras/protocol";
 export * from "@ty-ras/client-fetch";
 export * from "@ty-ras/data";
 export * from "@ty-ras/data-io-ts";
 export * from "@ty-ras/data-frontend";
 export * from "@ty-ras/data-frontend-io-ts";
+
+// Export "glue" code, needed because e.g. IO-TS FE library is not aware of the client being used.
+export * from "./api-call";
