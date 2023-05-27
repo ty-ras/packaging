86e8b02d08e2b0295c0903d61c8bd18589776445
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2023-05-27 12:36:16 +0300

    #42 Updating resource-pool and typed-sql dependen…
    …cies.

--- extras-backend-io-ts/src/index.ts
+++ extras-backend-io-ts/src/index.ts
@@ -6,8 +6,8 @@
 
 import * as configString from "@ty-ras-extras/config-io-ts/string";
 import * as configMaybeFile from "@ty-ras-extras/config-io-ts/maybe-file";
-
 export const configuration = { ...configString, ...configMaybeFile };
+
 export * as cache from "@ty-ras-extras/cache";
 export * as resources from "@ty-ras-extras/resource-pool-fp-ts";
 export * as sql from "@ty-ras-extras/typed-sql-io-ts";
