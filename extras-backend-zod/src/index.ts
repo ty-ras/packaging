d04601fb768a94b332d7459999f4c284d6d786ca
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2023-08-09 18:54:24 +0300

    #64 Updating 'zod' -specific extras packages to 2…
    ….0.0.

--- extras-backend-zod/src/index.ts
+++ extras-backend-zod/src/index.ts
@@ -12,4 +12,3 @@ export * as cache from "@ty-ras-extras/cache";
 export * as resources from "@ty-ras-extras/resource-pool";
 export * as sql from "@ty-ras-extras/typed-sql-zod";
 export * as main from "@ty-ras-extras/main";
-export * as state from "@ty-ras-extras/state-zod";
