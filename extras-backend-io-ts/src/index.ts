a33cdd889a34301ff5769b0fd7680c89fc58cd40
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2023-08-09 14:33:36 +0300

    #64 Updating io-ts -specific extras packages to 2…
    ….0.0.

--- extras-backend-io-ts/src/index.ts
+++ extras-backend-io-ts/src/index.ts
@@ -12,4 +12,3 @@ export * as cache from "@ty-ras-extras/cache";
 export * as resources from "@ty-ras-extras/resource-pool-fp-ts";
 export * as sql from "@ty-ras-extras/typed-sql-io-ts";
 export * as main from "@ty-ras-extras/main-io-ts";
-export * as state from "@ty-ras-extras/state-io-ts";
