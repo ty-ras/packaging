21be1cf582ddf4476c715805386996dc2f49589c
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2023-08-11 22:41:36 +0300

    #73 Adding FE package for Runtypes.

---
+++ frontend-fetch-runtypes/src/__test__/backend/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @file This file exposes the built TyRAS callbacks that can be used to call the REST API endpoints in typesafe and easy manner.
+ */
+
+import greeting from "./endpoints/greeting";
+
+export default {
+  greeting,
+};
