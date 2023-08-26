358ba05b4f005f3204ee3ada7f7c0e1a5cd1f5a1
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2023-08-26 14:03:45 +0300

    #83 Updating scripts so that they work with new f…
    …older structure.

--- scripts/preamble.sh
+++ scripts/preamble.sh
@@ -11,8 +11,11 @@ if [ -z "${TYRAS_LIB_DIR}" ]; then
   echo 'Please specify library directory as argument' 1>&2
   exit 1
 fi
-
 shift
+
+if [[ "${TYRAS_LIB_DIR}" == code/* ]]; then
+  TYRAS_LIB_DIR="${TYRAS_LIB_DIR:5}"
+fi
 
 yarn ()
 {
@@ -22,7 +25,7 @@ yarn ()
     --volume "${TYRAS_ROOT_DIR}/code/:${TYRAS_ROOT_DIR}/code/:rw" \
     --volume "${TYRAS_ROOT_DIR}/.yarn/:/yarn_dir/:rw" \
     --entrypoint yarn \
-    --workdir "${TYRAS_ROOT_DIR}/${TYRAS_LIB_DIR}" \
+    --workdir "${TYRAS_ROOT_DIR}/code/${TYRAS_LIB_DIR}" \
     --env YARN_CACHE_FOLDER="/yarn_dir/" \
     --env NODE_PATH="${TYRAS_ROOT_DIR}/code/${TYRAS_LIB_DIR}/node_modules" \
     "node:${TYRAS_NODE_VERSION}" \
