34130fced98c5f1285a09d6ad0852e5667ba04bf
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2024-02-03 22:35:58 +0200

    #178 Apparently old versions were forgotten to 'p…
    …ackaging/code/extras-frontend-io-ts/package.json', fixing that now.

--- code/extras-frontend-zod/src/index.ts
+++ code/extras-frontend-zod/src/index.ts
@@ -4,5 +4,5 @@
  * - using `zod` as data validation framework
  */
 
-export * as configuration from "@ty-ras-extras/config-zod/string";
+export * as configuration from "@ty-ras-extras/config-zod/string"; // Don't export maybe-file, as that uses Node-specific file operations.
 export * as cache from "@ty-ras-extras/cache";
